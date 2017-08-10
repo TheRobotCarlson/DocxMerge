30e251a518e9858d46e088b3ef9c53d8ab94804f
TheRobotCarlson <briancarlson6174@gmail.com>
2017-08-10 13:52:39 -0400

    prepared for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(
-  name = 'Confluency',
-  packages = ['Confluency'], # this must be the same as the name above
-  version = '0.4.3',
-  description = 'A simple library for accessing the Confluence REST API',
+  name = 'DocxMerge',
+  packages = ['DocxMerge'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple library for merging Microsoft Word Documents.',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
-  url = 'https://github.com/TheRobotCarlson/Confluency', # use the URL to the github repo
-  download_url = 'https://github.com/TheRobotCarlson/Confluency/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['confluence', 'rest', 'api'], # arbitrary keywords
+  url = 'https://github.com/TheRobotCarlson/DocxMerge', # use the URL to the github repo
+  download_url = 'https://github.com/TheRobotCarlson/DocxMerge/archive/0.1.tar.gz', # 
+  keywords = ['docx', 'merge', 'word'], # arbitrary keywords
 )
