d62ce5faadfbd9a49ea0f555b950e8821dba5649
TheRobotCarlson <briancarlson6174@gmail.com>
2017-08-10 14:45:17 -0400

    fixed stuff for PyPI

---
+++ DocxMerge/__init__.py
@@ -0,0 +1,1 @@
+from .DocxMerge import merge_docs
--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 setup(
   name = 'DocxMerge',
   packages = ['DocxMerge'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.2',
   description = 'A simple library for merging Microsoft Word Documents.',
   author = 'Brian Carlson',
   author_email = 'briancarlson6174@gmail.com',
   url = 'https://github.com/TheRobotCarlson/DocxMerge', # use the URL to the github repo
   download_url = 'https://github.com/TheRobotCarlson/DocxMerge/archive/0.1.tar.gz', # 
+  install_requires=['python-docx'],
   keywords = ['docx', 'merge', 'word'], # arbitrary keywords
 )
